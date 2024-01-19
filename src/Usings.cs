cebe9673053381764a026d3f8f20a8291dacf753
stratos <stratosblue@outlook.com>
2024-01-19 18:13:10 +0800

    code format

--- src/Usings.cs
+++ src/Usings.cs
@@ -1,6 +1,6 @@
 ﻿global using System;
-global using System.IO;
 global using System.Collections.Generic;
+global using System.IO;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
